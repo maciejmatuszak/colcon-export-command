717c5c99d8afa33f97aedc1bd67620f55196d372
Maciej Matuszak <maciej.matuszak@csiro.au>
2021-11-25 09:04:00 +1100

    clenup

--- build/lib/colcon_export_command/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2021 Maciej Matuszak
-# Licensed under the Apache License, Version 2.0
-
-__version__ = '0.0.1'
--- build/lib/colcon_export_command/event_handler/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Environment variable to override the default executor"""
-from colcon_core.environment_variable import EnvironmentVariable
-
-COLCON_COMMAND_EXPORT_PATH_ENVIRONMENT_VARIABLE = EnvironmentVariable(
-        'COLCON_COMMAND_EXPORT_PATH',
-        'Overrides export path for "colcon-export-command" plugin')
